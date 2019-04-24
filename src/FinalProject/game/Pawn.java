9f1737484d249d2d571742abb8858c1e9cd003cd
Daniel Konečný <dkonecny97@gmail.com>
2019-04-25 00:03:47 +0200

    Pawn can move two squares from starting position

--- src/FinalProject/game/Pawn.java
+++ src/FinalProject/game/Pawn.java
@@ -14,11 +14,17 @@ public class Pawn extends UniversalFigure {
         if(destination.isEmpty()) {
             // White can move only up.
             if(this.isWhite()) {
-                return destination.getCol() == source.getCol() && destination.getRow() == source.getRow()+1;
+                return destination.getCol() == source.getCol() &&
+                        (destination.getRow() == source.getRow()+1 ||
+                                // Can move two squares from starting position.
+                                source.getRow() == 1 && destination.getRow() == source.getRow()+2);
             }
             // Black can move only down.
             else {
-                return destination.getCol() == source.getCol() && destination.getRow() == source.getRow()-1;
+                return destination.getCol() == source.getCol() &&
+                        (destination.getRow() == source.getRow()-1 ||
+                                // Can move two squares from starting position.
+                                source.getRow() == 6 && destination.getRow() == source.getRow()-2);
             }
         }
         // Capture opponent's figure.
